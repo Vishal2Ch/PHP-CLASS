f7c890d36274a878d13c8c2a04746b44aebc317a
VishalChauhan_2005 <154717358+Vishal2Ch@users.noreply.github.com>
2025-04-05 13:50:59 +0530

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,18 @@
+<?php
+$intro ="Hello my name is Vishal <br> 12 <br> strings";
+$num = 23345;
+$char = "These are the characters $num";
+echo $intro;
+echo "<br>";
+echo $num;
+echo "<br>";
+echo $char;
+
+$don = "mitthu don";
+echo "<br>";
+echo "sabse bada don kon $don";
+?>
+
+<!-- In this file i am introducing the concept of variables 
+ Firstly i defined variables in different kinds of datatypes eg. numbers, characters 
+ and then i use a single variable to define in multiple print lines (echo)  -->
